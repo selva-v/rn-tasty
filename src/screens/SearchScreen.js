083d6873e9df31a4c4c2a867314fdd0f4268862b
selvakumar-v <selva.va@yahoo.com>
2020-02-21 16:40:52 +0800

    Search api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,37 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term, // term: term - is shorten with ES2015
+          limit: 50,
+          location: 'Singapore'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage('Something went wrong')
+    }
+  }
+
   return (
     <View style={styles.searchScreen}>
       <SearchBar
         term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log(term)}
+        onTermChange={setTerm} //newTerm => setTerm(newTerm) - shorten with ES2015
+        onTermSubmit={searchApi}
       />
-      <Text>{term}</Text>
-      
+      <Text>{results.length}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
     </View>
   )
 };
